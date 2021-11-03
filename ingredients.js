01fccb45bb04947846d3b67f8126f42ff23b6f27
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2021-11-03 23:13:11 +0000

    ingredients update

---
+++ ingredients.js
@@ -0,0 +1,11 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var item = 0
+
+while (item < ingredients.length) {
+  console.log(ingredients[item])
+  item++;
+}
+for (var item = 0; item < 7; item++)
+  console.log(ingredients[item]);
+for (var item = 7; item >= 0; item--)
+  console.log(ingredients[item]);
